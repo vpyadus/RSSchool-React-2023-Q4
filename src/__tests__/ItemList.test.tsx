2d7b223a026b404bb8203d850f41ab523da79b3b
vpyadus <41780056+vpyadus@users.noreply.github.com>
2023-11-12 00:49:25 +0300

    feat: add mock data and ItemList tests

---
+++ src/__tests__/ItemList.test.tsx
@@ -0,0 +1,26 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import ItemList from '../components/ItemList';
+import { items } from './mocks';
+import { BeerDetails } from '../api/BeerAPI';
+
+const noopFunc: (param?: unknown) => void = () => {};
+
+describe('ItemList', () => {
+  it('Renders list of 10 items', () => {
+    render(
+      <ItemList
+        items={items.filter((_, index) => index < 10)}
+        onItemSelect={noopFunc}
+      />
+    );
+    expect(screen.getAllByRole('heading', { level: 3 })).toHaveLength(10);
+  });
+
+  it('Shows Nothing found message if no cards are present', () => {
+    render(
+      <ItemList items={[] as Array<BeerDetails>} onItemSelect={noopFunc} />
+    );
+    expect(screen.getByText('Nothing found')).toBeInTheDocument();
+  });
+});
---
+++ src/__tests__/mocks.ts
@@ -0,0 +1,176 @@
+import { BeerDetails } from '../api/BeerAPI';
+
+export const items: Array<BeerDetails> = [
+  {
+    id: 2,
+    name: 'Trashy Blonde',
+    tagline: "You Know You Shouldn't",
+    description:
+      'A titillating, neurotic, peroxide punk of a Pale Ale. Combining attitude, style, substance, and a little bit of low self esteem for good measure; what would your mother say? The seductive lure of the sassy passion fruit hop proves too much to resist. All that is even before we get onto the fact that there are no additives, preservatives, pasteurization or strings attached. All wrapped up with the customary BrewDog bite and imaginative twist.',
+    image_url: 'https://images.punkapi.com/v2/2.png',
+    first_brewed: '04/2008',
+  },
+  {
+    id: 8,
+    name: 'Fake Lager',
+    tagline: 'Bohemian Pilsner.',
+    description:
+      'Fake is the new black. Fake is where it is at. Fake Art, fake brands, fake breasts, and fake lager. We want to play our part in the ugly fallout from the Lager Dream. Say hello to Fake Lager – a zesty, floral 21st century faux masterpiece with added BrewDog bitterness.',
+    image_url: 'https://images.punkapi.com/v2/8.png',
+    first_brewed: '03/2013',
+  },
+  {
+    id: 18,
+    name: 'Russian Doll – India Pale Ale',
+    tagline: 'Nesting Hop Bomb.',
+    description:
+      'The levels of hops vary throughout the range. We love hops, so all four beers are big, bitter badasses, but by tweaking the amount of each hop used later in the boil and during dry- hopping, we can balance the malty backbone with some unexpected flavours. Simcoe is used in the whirlpool for all four beers, and yet still lends different characters to each',
+    image_url: 'https://images.punkapi.com/v2/18.png',
+    first_brewed: '08/2014',
+  },
+  {
+    id: 36,
+    name: 'Shipwrecker Circus (w/ Oskar Blues)',
+    tagline: 'American Barley Wine - Oskar Blues Collab.',
+    description:
+      'In collaboration with the awesome Oskar Blues from Colorado, we created this big, twisted 10.5% American style barley wine. Big boozy aromas and a sweet viscosity, packed in with some heavy-hitting C-hops, for an intense bitterness to balance this full-bodied malt monster.',
+    image_url: 'https://images.punkapi.com/v2/36.png',
+    first_brewed: '09/2013',
+  },
+  {
+    id: 46,
+    name: 'Anarchist Alchemist',
+    tagline: 'Triple Hopped Triple Ipa.',
+    description:
+      'About as far as you can push an IPA; Anarchist Alchemist packs in three times the malt and three of our favourite hops. Nelson Sauvin, Amarillo and Centennial bring a range of flavours, touching on tropical fruit, white grape, tangerine, grapefruit, pine, spiced orange... the list goes on!',
+    image_url: 'https://images.punkapi.com/v2/46.png',
+    first_brewed: '03/2012',
+  },
+  {
+    id: 55,
+    name: 'Amarillo - IPA Is Dead',
+    tagline: 'Single Hop India Pale Ale.',
+    description:
+      'Citrus fruit. Lots of citrus fruit. Think blood orange, orange zest and grapefruit, tangerine and lemon; the fruit is balanced by floral notes in the most classic of new wave IPA hops. Amarillo adds a zesty, pithy character that sits beautifully alongside hints of toffee and honeycomb.',
+    image_url: 'https://images.punkapi.com/v2/55.png',
+    first_brewed: '01/2013',
+  },
+  {
+    id: 67,
+    name: 'Hunter Foundation Pale Ale',
+    tagline: 'American Pale Ale.',
+    description:
+      "This straight-up US style pale ale uses some of our favourite hops against a toasty malt base. We brewed this with Sir Tom Hunter at our Fraserburgh brewery - to add a charitable element to your own version, feel free to share this citrusy pale ale with your friends. Or don't, it's your choice.",
+    image_url: 'https://images.punkapi.com/v2/67.png',
+    first_brewed: '05/2012',
+  },
+  {
+    id: 71,
+    name: 'Pale - Russian Doll',
+    tagline: 'Nesting Hop Bomb.',
+    description:
+      'The levels of hops vary throughout the range. We love hops, so all four beers are big, bitter badasses, but by tweaking the amount of each hop used later in the boil and during dry- hopping, we can balance the malty backbone with some unexpected flavours. Simcoe is used in the whirlpool for all four beers, and yet still lends different characters to each.',
+    image_url: 'https://images.punkapi.com/v2/71.png',
+    first_brewed: '08/2014',
+  },
+  {
+    id: 81,
+    name: 'India Session Lager - Prototype Challenge',
+    tagline: 'Refreshing Hop Fix.',
+    description:
+      'BrewDog’s level of dry-hop to a beer formed with a baseline of 100% pilsner malt – and at under 4.5% ABV – gives you a style that flirts at the edges of several others. Think aromas of fresh cut grass, nettle, white grape, melon, tangerine - with similar flavours leading to a dry, bitter finish.',
+    image_url: 'https://images.punkapi.com/v2/keg.png',
+    first_brewed: '11/2015',
+  },
+  {
+    id: 82,
+    name: 'Hopped-Up Brown Ale - Prototype Challenge',
+    tagline: 'Malt Hop Balance.',
+    description:
+      'Brown ales are perfect foils for resinous C-Hops, as the piney elements of the latter contrast brilliantly with the sweeter, nutty elements of the malt bill. The best of both worlds.',
+    image_url: 'https://images.punkapi.com/v2/82.png',
+    first_brewed: '01/2016',
+  },
+  {
+    id: 11,
+    name: 'Misspent Youth',
+    tagline: 'Milk & Honey Scotch Ale.',
+    description:
+      'The brainchild of our small batch brewer, George Woods. A dangerously drinkable milk sugar- infused Scotch Ale.',
+    image_url: 'https://images.punkapi.com/v2/keg.png',
+    first_brewed: '04/2013',
+  },
+  {
+    id: 12,
+    name: 'Arcade Nation',
+    tagline: 'Seasonal Black IPA.',
+    description:
+      'Running the knife-edge between an India Pale Ale and a Stout, this particular style is one we truly love. Black IPAs are a great showcase for the skill of our brew team, balancing so many complex and twisting flavours in the same moment. The citrus, mango and pine from the hops – three of our all-time favourites – play off against the roasty dryness from the malt bill at each and every turn.',
+    image_url: 'https://images.punkapi.com/v2/12.png',
+    first_brewed: '12/2015',
+  },
+  {
+    id: 13,
+    name: 'Movember',
+    tagline: 'Moustache-Worthy Beer.',
+    description:
+      'A deliciously robust, black malted beer with a decadent dark, dry cocoa flavour that provides an enticing backdrop to the Cascade hops.',
+    image_url: 'https://images.punkapi.com/v2/13.png',
+    first_brewed: '11/2009',
+  },
+  {
+    id: 14,
+    name: 'Alpha Dog',
+    tagline: 'Existential Red Ale.',
+    description:
+      'A fusion of caramel malt flavours and punchy New Zealand hops. A session beer you can get your teeth into.',
+    image_url: 'https://images.punkapi.com/v2/14.png',
+    first_brewed: '02/2010',
+  },
+  {
+    id: 15,
+    name: 'Mixtape 8',
+    tagline:
+      'An Epic Fusion Of Old Belgian, American New Wave, And Scotch Whisky.',
+    description:
+      'This recipe is for the Belgian Tripel base. A blend of two huge oak aged beers – half a hopped up Belgian Tripel, and half a Triple India Pale Ale. Both aged in single grain whisky barrels for two years and blended, each beer brings its own character to the mix. The Belgian Tripel comes loaded with complex spicy, fruity esters, and punchy citrus hop character.',
+    image_url: 'https://images.punkapi.com/v2/15.png',
+    first_brewed: '01/2012',
+  },
+  {
+    id: 16,
+    name: 'Libertine Porter',
+    tagline: 'Dry-Hopped Aggressive Porter.',
+    description:
+      'An avalanche of cross-continental hop varieties give this porter a complex spicy, resinous and citrusy aroma, with a huge malt bill providing a complex roasty counterpoint. Digging deeper into the flavour draws out cinder toffee, bitter chocolate and hints of woodsmoke.',
+    image_url: 'https://images.punkapi.com/v2/16.png',
+    first_brewed: '01/2012',
+  },
+  {
+    id: 17,
+    name: 'AB:06',
+    tagline: 'Imperial Black IPA.',
+    description:
+      'Our sixth Abstrakt, this imperial black IPA combined dark malts with a monumental triple dry-hop, using an all-star team of some of our favourite American hops. Roasty and resinous.',
+    image_url: 'https://images.punkapi.com/v2/17.png',
+    first_brewed: '04/2011',
+  },
+  {
+    id: 19,
+    name: 'Hello My Name Is Mette-Marit',
+    tagline: 'Lingonberry Double IPA.',
+    description:
+      "We sent this beer to Norway where it was known as 'Hello, my name is Censored’. You can make up your own mind as to why. This brew was a red berry explosion, with a reisnous bitter edge layered with dry berry tartness.",
+    image_url: 'https://images.punkapi.com/v2/19.png',
+    first_brewed: '06/2013',
+  },
+  {
+    id: 20,
+    name: 'Rabiator',
+    tagline: 'Imperial Wheat Beer',
+    description:
+      'Imperial Wheat beer / Weizenbock brewed by a homesick German in leather trousers. Think banana bread, bubble gum and David Hasselhoff.',
+    image_url: 'https://images.punkapi.com/v2/keg.png',
+    first_brewed: '03/2011',
+  },
+];
