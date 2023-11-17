60f8e27b8b9cde7c7026fc90b0e4bfc7162331ce
vpyadus <41780056+vpyadus@users.noreply.github.com>
2023-11-18 01:27:25 +0300

    refactor: redistribute logic in App and Pagination components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, StoreState } from './store/store';
 import Search, { SearchHandlerFunc } from './components/Search';
 import ShowErrorButton from './components/ShowErrorButton';
 import { Outlet, useSearchParams } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import upsertSearchParam from './utils/upsertSearchParam';
 import removeSearchParam from './utils/removeSearchParam';
-import { setItemsPerPage } from './features/itemsPerPageSlice/itemsPerPageSlice';
 
 export interface OutletContextParams {
   itemId: string;
@@ -15,22 +11,9 @@ export interface OutletContextParams {
 }
 
 const App = () => {
-  const perPage: number = useSelector(
-    (state: StoreState) => state.perPage.perPage
-  );
-  const dispatch: AppDispatch = useDispatch();
-  const setPerPage = (newPerPage: number) =>
-    dispatch(setItemsPerPage(newPerPage));
-
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const currentItemId = searchParams.get('details') ?? '';
-  const currentPageNum = searchParams.get('page') ?? '1';
-
-  const [page, setPage] = useState<number>(Number(currentPageNum));
-  const [selectedItemId, setSelectedItemId] = useState<number>(
-    Number(currentItemId)
-  );
+  const itemId = searchParams.get('details') ?? '';
 
   const afterSearchHandler: SearchHandlerFunc = (): void => {
     const updatedParams: URLSearchParams = upsertSearchParam(
@@ -39,17 +22,6 @@ const App = () => {
       '1'
     );
     setSearchParams(updatedParams);
-    setPage(1);
-  };
-
-  const pageChangeHandler = (newPage: number): void => {
-    const updatedParams: URLSearchParams = upsertSearchParam(
-      searchParams,
-      'page',
-      String(newPage)
-    );
-    setSearchParams(updatedParams);
-    setPage(newPage);
   };
 
   const onItemSelect = (itemId: number): void => {
@@ -59,13 +31,11 @@ const App = () => {
       String(itemId)
     );
     setSearchParams(updatedParams);
-    setSelectedItemId(itemId);
   };
 
   const hideItemDetails = (): void => {
     const updatedParams = removeSearchParam(searchParams, 'details');
     setSearchParams(updatedParams);
-    setSelectedItemId(0);
   };
 
   return (
@@ -86,21 +56,13 @@ const App = () => {
           padding: '10px',
         }}
       >
-        <MainPage
-          {...{
-            page,
-            setPage: pageChangeHandler,
-            perPage,
-            setPerPage,
-            onItemSelect,
-          }}
-        />
+        <MainPage onItemSelect={onItemSelect} />
       </main>
-      {!!selectedItemId && (
+      {!!itemId && (
         <Outlet
           context={
             {
-              itemId: String(selectedItemId),
+              itemId,
               hideItemDetails,
             } as OutletContextParams
           }
--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,15 +1,43 @@
 import { ChangeEvent } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, StoreState } from '../../store/store';
+import { setItemsPerPage } from '../../features/itemsPerPageSlice/itemsPerPageSlice';
+import { useSearchParams } from 'react-router-dom';
+import upsertSearchParam from '../../utils/upsertSearchParam';
 
 export interface PaginationProps {
-  page: number;
-  perPage: number;
-  onPageChange: (newPage: number) => void;
-  onPerPageChange: (newPerPage: number) => void;
   isLastPage: boolean;
 }
 
 const Pagination = (props: PaginationProps) => {
-  const { page, perPage, onPageChange, onPerPageChange, isLastPage } = props;
+  const { isLastPage } = props;
+
+  // the URL query is the source of truth for a page number
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page: number = Number(searchParams.get('page')) ?? 1;
+
+  const pageChangeHandler = (newPage: number): void => {
+    const updatedParams: URLSearchParams = upsertSearchParam(
+      searchParams,
+      'page',
+      String(newPage)
+    );
+    setSearchParams(updatedParams);
+  };
+
+  // the store is the source of truth for the number of items per page
+  const perPage: number = useSelector(
+    (state: StoreState) => state.perPage.perPage
+  );
+  const dispatch: AppDispatch = useDispatch();
+
+  const handlePerPageChange: (e: ChangeEvent<HTMLSelectElement>) => void = (
+    e
+  ) => {
+    dispatch(setItemsPerPage(e.target.value));
+    pageChangeHandler(1);
+  };
+
   return (
     <div
       style={{
@@ -20,12 +48,7 @@ const Pagination = (props: PaginationProps) => {
     >
       <div style={{ display: 'flex', gap: '10px' }}>
         <label>Per page:</label>
-        <select
-          defaultValue={String(perPage)}
-          onChange={(e: ChangeEvent<HTMLSelectElement>) => {
-            onPerPageChange(Number(e.target.value));
-          }}
-        >
+        <select defaultValue={String(perPage)} onChange={handlePerPageChange}>
           <option value="5">5</option>
           <option value="10">10</option>
           <option value="25">25</option>
@@ -40,10 +63,16 @@ const Pagination = (props: PaginationProps) => {
           flexGrow: '2',
         }}
       >
-        <button disabled={page === 1} onClick={() => onPageChange(page - 1)}>
+        <button
+          disabled={page === 1}
+          onClick={() => pageChangeHandler(page - 1)}
+        >
           Prev Page
         </button>
-        <button disabled={isLastPage} onClick={() => onPageChange(page + 1)}>
+        <button
+          disabled={isLastPage}
+          onClick={() => pageChangeHandler(page + 1)}
+        >
           Next Page
         </button>
       </div>
